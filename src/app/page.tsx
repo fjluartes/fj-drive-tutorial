c6732dceeccc14494c8bec0765450521250cd500
Fred Luartes <fjluartes@gmail.com>
2025-02-06 21:39:09 +0800

    Edits page.tsx fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-
-  void api.post.getLatest.prefetch();
 
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          You&apos;re amazing app is here    
+          You&apos;re amazing app is here
         </div>
       </main>
     </HydrateClient>
