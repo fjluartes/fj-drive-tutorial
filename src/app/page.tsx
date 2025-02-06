798720c1ee675de2b98bb9c75bfb0ef5398e4bce
Fred Luartes <fjluartes@gmail.com>
2025-02-06 21:26:24 +0800

    Ignore eslint, typescript errors next config, edits page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import Link from "next/link";
-
-import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
 
   void api.post.getLatest.prefetch();
 
@@ -12,7 +8,7 @@ export default async function Home() {
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          You&apos;re amazing app is here          
+          You&apos;re amazing app is here    
         </div>
       </main>
     </HydrateClient>
