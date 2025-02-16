1bb6f6e6e81eb379c30395a5b1a4dc98e150cdbd
Fred <fjluartes@gmail.com>
2025-02-16 15:58:35 +0800

    Adds drizzle/singlestore

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,14 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
+  dialect: "singlestore",
   tablesFilter: ["fj-drive-tutorial_*"],
+  dbCredentials: {
+    host: env.SINGLESTORE_HOST,
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+    maxIdle: 0,
+  },
 } satisfies Config;
